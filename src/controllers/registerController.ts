36c1f11109e06883c13552fbb5762dbcf9b26cd2
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-09-30 16:11:42 -0300

    criado controller para registro de usuário

---
+++ src/controllers/registerController.ts
@@ -0,0 +1,22 @@
+import { RequestHandler, Response } from 'express';
+import User from '../models/User';
+import { RequestWithUser } from '../interfaces/request-with-user-and-session';
+
+export const register: RequestHandler = async (req, res: Response) => {
+	try {
+		const user = new User(req.body);
+
+		await user.register();
+
+		if (user.errors.length > 0) {
+			return res.status(400).json({ error: user.errors });
+		}
+
+		return res
+			.status(200)
+			.json({ message: 'Usuário criado com sucesso. Faça seu login.' });
+	} catch (err) {
+		console.log('Erro no registro do usuário:', err);
+		res.status(500).json({ error: 'Erro interno do servidor' });
+	}
+};
