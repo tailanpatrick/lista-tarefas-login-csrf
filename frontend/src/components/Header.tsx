062c2b4b13bef523709f4227743c6952a2048ff1
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-10-04 17:36:36 -0300

    removendo importações não utilizadas solucionando errors no deploy

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { CgLogOff } from 'react-icons/cg';
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
 const Header: React.FC = () => {
