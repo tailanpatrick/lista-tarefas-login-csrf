8f80d801f91efe476863866e0f4806b43b1c9d36
Tailan Patrick <146599324+tailanpatrick@users.noreply.github.com>
2025-10-07 17:14:10 -0300

    ajuste  geral para deploy

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://localhost:3000/api',
+	baseURL: 'lista-tarefas-login-csrf.vercel.app/api',
 	withCredentials: true,
 });
 
